198cc16b20707e90d2f6abced29df553b7ee1b70
Andrew Armaneous <andrew@cloudcoreo.com>
2017-11-20 13:49:18 -0800

    Created CloudCoreoProjectAction

---
+++ src/main/java/com/cloudcoreo/plugins/jenkins/CloudCoreoProjectAction.java
@@ -0,0 +1,20 @@
+package com.cloudcoreo.plugins.jenkins;
+
+import hudson.model.Action;
+
+public class CloudCoreoProjectAction implements Action {
+    @Override
+    public String getIconFileName() {
+        return null;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return null;
+    }
+
+    @Override
+    public String getUrlName() {
+        return null;
+    }
+}
