801cb18cab0ecd52ecf8e15cd50e3073ae228f0f
Andrew Armaneous <andrew@cloudcoreo.com>
2017-11-24 13:21:13 -0800

    Some basics for CloudCoreoProjectAction

--- src/main/java/com/cloudcoreo/plugins/jenkins/CloudCoreoProjectAction.java
+++ src/main/java/com/cloudcoreo/plugins/jenkins/CloudCoreoProjectAction.java
@@ -1,8 +1,23 @@
 package com.cloudcoreo.plugins.jenkins;
 
+import hudson.model.AbstractProject;
 import hudson.model.Action;
+import hudson.model.Job;
 
 public class CloudCoreoProjectAction implements Action {
+
+    private final AbstractProject<?, ?> project;
+    private final Job<?, ?> job;
+
+    public CloudCoreoProjectAction(AbstractProject<?, ?> project) {
+        this((Job) project);
+    }
+
+    public CloudCoreoProjectAction(Job<?, ?> job) {
+        this.job = job;
+        project = job instanceof AbstractProject ? (AbstractProject) job : null;
+    }
+
     @Override
     public String getIconFileName() {
         return null;
