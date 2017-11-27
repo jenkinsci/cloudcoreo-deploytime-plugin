7fed79aabec873926917dc1e9968e6afb0ef2991
Andrew Armaneous <andrew@cloudcoreo.com>
2017-11-26 20:02:41 -0800

    Added retrieving results from project action

--- src/main/java/com/cloudcoreo/plugins/jenkins/CloudCoreoProjectAction.java
+++ src/main/java/com/cloudcoreo/plugins/jenkins/CloudCoreoProjectAction.java
@@ -3,11 +3,18 @@ package com.cloudcoreo.plugins.jenkins;
 import hudson.model.AbstractProject;
 import hudson.model.Action;
 import hudson.model.Job;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import java.io.IOException;
 
 public class CloudCoreoProjectAction implements Action {
 
+    private final int RESULT_LIMIT = 30;
     private final AbstractProject<?, ?> project;
     private final Job<?, ?> job;
+    private JSONArray results;
+    private JSONObject lastResult;
 
     public CloudCoreoProjectAction(AbstractProject<?, ?> project) {
         this((Job) project);
@@ -15,7 +22,9 @@ public class CloudCoreoProjectAction implements Action {
 
     public CloudCoreoProjectAction(Job<?, ?> job) {
         this.job = job;
-        project = job instanceof AbstractProject ? (AbstractProject) job : null;
+        project = (job instanceof AbstractProject) ? (AbstractProject) job : null;
+        results = null;
+        lastResult = new JSONObject();
     }
 
     @Override
@@ -25,11 +34,47 @@ public class CloudCoreoProjectAction implements Action {
 
     @Override
     public String getDisplayName() {
-        return null;
+        return "CloudCoreo DeployTime Results";
     }
 
     @Override
     public String getUrlName() {
-        return null;
+        return "cloudcoreo-deploytime";
+    }
+
+    public JSONArray getAllBuildResults() throws IOException {
+        results = ResultManager.getAllResults(this.project.getWorkspace());
+        if (results.size() > RESULT_LIMIT) {
+            int lastIndex = results.size() - 1;
+            results = JSONArray.fromObject(results.subList(lastIndex - RESULT_LIMIT, lastIndex));
+        }
+        return results;
+    }
+
+    public JSONObject getLastBuildResult() throws IOException {
+        results = getAllBuildResults();
+        if (results.size() > 0) {
+            lastResult = results.getJSONObject(results.size() - 1);
+        }
+
+        return lastResult;
+    }
+
+    public int getLastCount(String level) throws IOException {
+        JSONObject violations = getLastBuildResult().getJSONObject("violations");
+        level = level.toUpperCase();
+        if (violations.containsKey(level)) {
+            return violations.getJSONArray(level).size();
+        }
+        return 0;
+    }
+
+    public int getLastTotalCount() throws IOException {
+        String[] levels = {"LOW", "MEDIUM", "HIGH"};
+        int total = 0;
+        for (String level : levels) {
+            total += getLastCount(level);
+        }
+        return total;
     }
 }
