16dd895900e6b3a5a1ee3598751d1d1218bd9fb5
Andrew Armaneous <andrew@cloudcoreo.com>
2018-05-07 12:11:47 -0700

    Ensuring results are written before attempting to count results

--- src/test/java/com/cloudcoreo/plugins/jenkins/CloudCoreoProjectActionTest.java
+++ src/test/java/com/cloudcoreo/plugins/jenkins/CloudCoreoProjectActionTest.java
@@ -1,16 +1,19 @@
 package com.cloudcoreo.plugins.jenkins;
 
+import com.cloudcoreo.plugins.jenkins.exceptions.EndpointUnavailableException;
 import hudson.FilePath;
 import hudson.model.AbstractProject;
 import hudson.model.FreeStyleProject;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-
-import java.io.File;
+import org.jvnet.hudson.test.JenkinsRule;
 import java.io.IOException;
 
 public class CloudCoreoProjectActionTest {
+    @Rule
+    public JenkinsRule rule = new JenkinsRule();
     private CloudCoreoProjectAction projectAction;
 
     private class ProjectStub extends CloudCoreoProjectAction {
@@ -25,9 +28,18 @@ public class CloudCoreoProjectActionTest {
     }
 
     @Before
-    public void setUp() {
+    public void setUp() throws IOException, EndpointUnavailableException {
         AbstractProject project = new FreeStyleProject(null, "myProject");
         projectAction = new ProjectStub(project);
+
+        CloudCoreoTeam team = new CloudCoreoTeamTest.CloudCoreoTeamStub();
+        team.getDeployTime().setDeployTimeId("myContext", "myTask");
+
+        ResultManager manager = new ResultManagerTest.ResultManagerStub(false, true, true);
+        manager.setResults(team, ResultManagerTest.BUILD_ID);
+
+        FilePath testFilePath = CloudCoreoPublisherTest.BUILD_PATH;
+        manager.writeResultsToFile(testFilePath, ResultManagerTest.BUILD_ID);
     }
 
     @Test
